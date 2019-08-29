5ef9efb4adcc18f259065e28b7590af3763bd828
SRIRAM GOPALAKRISHNAN <45805073+SriramGopalakrishnan04@users.noreply.github.com>
2019-08-30 03:38:43 +0530

    third commit

--- src/webparts/helloSpFx/components/HelloSpFx.tsx
+++ src/webparts/helloSpFx/components/HelloSpFx.tsx
@@ -10,7 +10,7 @@ export default class HelloSpFx extends React.Component<IHelloSpFxProps, {}> {
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint Online!</span>
+              <span className={ styles.title }>Hello,Welcome to SharePoint Online!</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
